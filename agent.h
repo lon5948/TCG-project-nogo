ec9cfbfd05e31d12aa69850a1891c1460f0fa5bb
lon5948 <a0988282303@gmail.com>
2022-11-20 02:04:35 +0800

    against strong AI - winrate 50%

--- agent.h
+++ agent.h
@@ -69,10 +69,7 @@ protected:
 	std::default_random_engine engine;
 };
 
-/**
- * random player for both side
- * put a legal piece randomly
- */
+
 class Node {
 	public:
         int total = 0; 
@@ -108,36 +105,50 @@ public:
 
 	virtual action take_action(const board& state) {
         if (search == "mcts") {
+			/*
+			 * mcts player with time management 
+			 */
             clock_t start_time = clock();
             clock_t end_time;
 
             int total_count = 0;
+			/* find out what step it is */
             int step = 73;
             for(int i = 0; i < 9; i++){
                 for(int j = 0; j < 9; j++){
                     if(state[i][j] == board::empty) step--;
                 }
             }
+			/* It's opponent's turn */
             Node* root = new Node;
             root->state = state;
             root->who = (who == board::white ? board::black : board::white);
             Expansion(root);
+
+			bool win;
+			board::piece_type winner;
+			Node* selected_node;
+			/* If total time exceeds the limit time in this step, stop doing MCTS. */
             do {
-				total_count++;
-                Node* selected_node = Selection(root);
+				total_count++; /* record visit count in total */
+                selected_node = Selection(root);
                 Expansion(selected_node);
-                board::piece_type winner = Simulation(selected_node);
-                bool win = (root->who != winner);
+                winner = Simulation(selected_node);
+                win = (root->who != winner); 
                 BackPropogation(root, selected_node, win, total_count);
                 end_time = clock();
             } while((double)(end_time - start_time)/CLOCKS_PER_SEC < time_management[step/2]);
-            
+            /* according to nodes’ # of totals, return the best action. */
 			action best_action = get_best_action(root);
             delete_tree(root);
             delete(root);
             return best_action;
         }
         else {
+			/**
+			 * random player for both side
+			 * put a legal piece randomly
+			 */
             std::shuffle(space.begin(), space.end(), engine);
             for (const action::place& move : space) {
                 board after = state;
@@ -149,9 +160,11 @@ public:
 	}
 
     Node* Selection(Node* node){
+		/* return leaf node */
 		while(!node->children.empty()){
 			double max_ucb = -1;
 			int max_ucb_ind= 0; 
+			/* select the child which has max ucb score */
 			for(size_t i=0; i < node->children.size(); i++){
 				if (node->children[i]->ucb > max_ucb){
 					max_ucb = node->children[i]->ucb;
@@ -165,6 +178,7 @@ public:
 
     void Expansion(Node* parent_node){
 		if (parent_node->who == board::black){
+			/* expand all the valid moves in white space */
 			for (size_t i = 0; i < white_space.size(); i++){
 				board after = parent_node->state;
 				if (white_space[i].apply(after) == board::legal){
@@ -178,6 +192,7 @@ public:
 			}
 		}
 		else if(parent_node->who == board::white){
+			/* expand all the valid moves in black space */
 			for (size_t i = 0; i < black_space.size(); i++){
 				board after = parent_node->state;
 				if (black_space[i].apply(after) == board::legal){
@@ -198,43 +213,44 @@ public:
 		board::piece_type who = node->who;
 		while(flag){
 			flag = false;
-            who = (who == board::white ? board::black : board::white);
+            who = (who == board::white ? board::black : board::white); /* whose turn */
 			if (who == board::black){
+				/* black randomly gets one of valid moves */
 				std::shuffle(black_space.begin(), black_space.end(), engine);
-				for (const action::place& move : black_space) {
+				for (size_t i = 0; i < black_space.size(); i++) {
 					board after = state;
-					if (move.apply(after) == board::legal){
-						move.apply(state);
+					if (black_space[i].apply(after) == board::legal){
+						black_space[i].apply(state);
 						flag = true;
 						break;
 					}
 				}
 			}
 			else if (who == board::white){
+				/* white randomly gets one of valid moves */
 				std::shuffle(white_space.begin(), white_space.end(), engine);
-				for (const action::place& move : white_space) {
+				for (size_t i = 0; i < white_space.size(); i++) {
 					board after = state;
-					if (move.apply(after) == board::legal){
-						move.apply(state);
+					if (white_space[i].apply(after) == board::legal){
+						white_space[i].apply(state);
 						flag = true;
 						break;
 					}
 				}
 			}
 		}
+		/* no legal action, so the rival wins */
         who = (who == board::white ? board::black : board::white);
-		return who;
-	}
-
-    double get_UCB_value(Node* node, int total_count){
-		return ((double)node->win/node->total) + constant * sqrt(log((double)total_count)/node->total);
+		return who; /* return the winner*/
 	}
 
     void BackPropogation(Node* root, Node* node, bool win, int total_count){
 		while(node != root){
+			/* update the node’s # of wins and # of totals */
 			node->total++;
 			if (win) node->win++;
-			node->ucb = get_UCB_value(node, total_count);
+			/* update the node's ucb */
+			node->ucb = ((double)node->win/(double)node->total) + constant * sqrt(log((double)total_count)/(double)node->total);
 			node = node->parent;
 		}
 	}
@@ -242,6 +258,7 @@ public:
     action get_best_action(Node* node){
 		action best_action = action();
 		int max_count = -1;
+		/* select the best action based on the visit count of child nodes. */
 		for(size_t i = 0; i < node->children.size(); i++){
 			if (node->children[i]->total > max_count){
 				max_count = node->children[i]->total;
@@ -267,7 +284,7 @@ private:
     std::vector<action::place> white_space;
     std::vector<action::place> black_space;
 	board::piece_type who;
-    double constant = 0.4;
+    double constant = 0.5;
     double time_management[36] = {0.5, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, 0.8, 
                                   1.0, 1.0, 1.0, 1.0, 1.5, 1.5, 1.5, 1.5, 
                                   2.0, 2.0, 2.0, 2.0, 1.5, 1.5, 1.5, 1.5, 
