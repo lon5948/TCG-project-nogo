6d7f88231e612839520976592cbee0c933b549de
lon5948 <a0988282303@gmail.com>
2023-01-10 14:59:17 +0800

    add RAVE

--- agent.h
+++ agent.h
@@ -11,17 +11,14 @@
 #include <string>
 #include <random>
 #include <sstream>
-#include <iostream>
-#include <cmath>
 #include <map>
-#include <vector>
 #include <type_traits>
 #include <algorithm>
 #include <fstream>
-#include <time.h>
-#include <omp.h>
 #include "board.h"
 #include "action.h"
+#include <omp.h>
+#include <thread>
 
 class agent {
 public:
@@ -56,9 +53,6 @@ protected:
 	std::map<key, value> meta;
 };
 
-/**
- * base agent for agents with randomness
- */
 class random_agent : public agent {
 public:
 	random_agent(const std::string& args = "") : agent(args) {
@@ -71,232 +65,247 @@ protected:
 	std::default_random_engine engine;
 };
 
-
-class Node {
+class node{
 	public:
-        int total = 0; 
-		int win = 0;
-		Node* parent = nullptr;
-		action::place move;
-		std::vector<Node*> children;
-        board state;
+		board state;
 		board::piece_type who;
-		~Node(){}
+		int win = 0;
+		int visit = 0;
+		//double UCB_RAVE_value = 0x7fffffff;
+		action::place move;
+		node* parent = nullptr;
+		std::vector<node*> children;
+		~node(){};
 };
 
-class player : public random_agent {
+class MCTS_player : public random_agent {
 public:
-	player(const std::string& args = "") : random_agent("name=random role=unknown " + args),
-		space(board::size_x * board::size_y), white_space(board::size_x * board::size_y),
-        black_space(board::size_x * board::size_y), who(board::empty) {
-		if (name().find_first_of("[]():; ") != std::string::npos)
-			throw std::invalid_argument("invalid name: " + name());
-        if (meta.find("search") != meta.end()) search = (std::string)meta["search"];
+	std::vector<action::place> space, white_space, black_space;
+	MCTS_player(const std::string& args = "") : random_agent("name=random role=unknown " + args),
+		space(board::size_x * board::size_y),white_space(board::size_x * board::size_y),
+		black_space(board::size_x * board::size_y), who(board::empty) {
+		if (meta.find("search") != meta.end()) search = (std::string)meta["search"];
+		if (meta.find("simulation") != meta.end()) simulation_count = (int)meta["simulation"];
+		if (meta.find("thread") != meta.end()) thread_num = (int)meta["thread"];
 		if (role() == "black") who = board::black;
 		if (role() == "white") who = board::white;
-		if (who == board::empty)
-			throw std::invalid_argument("invalid role: " + role());
 		for (size_t i = 0; i < space.size(); i++)
 			space[i] = action::place(i, who);
-        for (size_t i = 0; i < white_space.size(); i++)
+		for (size_t i = 0; i < white_space.size(); ++i)
 			white_space[i] = action::place(i, board::white);
-		for (size_t i = 0; i < black_space.size(); i++)
+		for (size_t i = 0; i < black_space.size(); ++i)
 			black_space[i] = action::place(i, board::black);
 	}
 
 	virtual action take_action(const board& state) {
-        if (search == "mcts") {
-			// mcts player with time management 
-			clock_t start_time = clock();
-			clock_t end_time;
-			// find out what step it is 
-			int step = 73;
-			for(int i = 0; i < 9; i++){
-				for(int j = 0; j < 9; j++){
-					if(state[i][j] == board::empty) step--;
+		if (search == "p-mcts"){
+			omp_set_num_threads(thread_num);
+			std::vector<node*> roots(thread_num);
+
+			#pragma omp parallel for
+			for(int i = 0; i < thread_num; i++) {
+				roots[i] = new node;
+				roots[i]->state = state;
+				roots[i]->who = (who == board::white ? board::black : board::white);
+				int total_node = 0;
+				Expansion(roots[i], total_node);
+				board::piece_type winner;
+				run_MCTS(roots[i], winner, total_node);	
+			}		
+
+			for (int idx = 1; idx < thread_num; idx++) {
+				for(size_t i = 0; i < roots[0]->children.size() ; i++) {
+					roots[0]->children[i]->visit += roots[idx]->children[i]->visit;
 				}
 			}
-			// It's opponent's turn 
-			Node* root = new Node;
-			root->state = state;
-			root->who = (who == board::white ? board::black : board::white);
-			Expansion(root);
 
-			bool win;
-			board::piece_type winner;
-			Node* selected_node;
-			// If total time exceeds the limit time in this step, stop doing MCTS. 
-			do {
-				root->total += 1;
-				selected_node = Selection(root);
-				Expansion(selected_node);
-				winner = Simulation(selected_node);
-				win = (root->who != winner); 
-				BackPropagation(root, selected_node, win);
-				end_time = clock();
-			} while((double)(end_time - start_time)/CLOCKS_PER_SEC < time_management[step/2]);	
-
-            // according to nodes’ # of totals, return the best action. 
-			action best_action = get_best_action(root);
-			// std::cout << best_action << std::endl;
-			delete_tree(root);
-            delete(root);
-            return best_action;
-        }
-        else {
-			// random player for both side put a legal piece randomly 
-            std::shuffle(space.begin(), space.end(), engine);
-            for (const action::place& move : space) {
-                board after = state;
-                if (move.apply(after) == board::legal)
-                    return move;
-            }
-        }
-		return action();
-	}
-
-    Node* Selection(Node* node){
-		// return leaf node 
-		while(!node->children.empty()){
-			double max_ucb = -1;
-			int max_ucb_ind = 0; 
-			// select the child which has max ucb score 
-			for(size_t i = 0; i < node->children.size(); i++){
-				double ucb = get_ucb_value(node->children[i]);
-				if (ucb > max_ucb){
-					max_ucb = ucb;
-					max_ucb_ind = i;
-				}
+			action best_action = get_action(roots[0]);
+			#pragma omp parallel for
+			for(int i = 0; i < thread_num; i++) {
+				delete_tree(roots[i]);
+				free(roots[i]);
 			}
-			node = node->children[max_ucb_ind];
+			return best_action;
 		}
-		return node;
+		else {
+			std::shuffle(space.begin(), space.end(), engine);
+			for (const action::place& move : space) {
+				board after = state;
+				if (move.apply(after) == board::legal)
+					return move;
+			}
+			return action();
+		}
 	}
 
-    void Expansion(Node* parent_node){
+	node* Selection(node* n) {			
+		node* cur = n;
+		while(!cur->children.empty()) {
+			double max_value = 0;
+			int select_idx = 0;
+			for(size_t i = 0; i < cur->children.size(); ++i) {
+				double ucb = get_ucb_value(cur->children[i]);
+				if(max_value < ucb) {
+					max_value = ucb;
+					select_idx = i;
+				}
+			}
+			cur = cur->children[select_idx];
+		}
+		return cur;
+	}
+
+	double get_ucb_value(node* cur) {					
+		if(cur->visit == 0 || rave_map[cur->move].first == 0) return 1e8;
+		double constant = std::sqrt(2);
+		double beta = std::sqrt((double) simulation_count/(double)(3 * count + simulation_count));
+		double win_rate = (double) cur->win / (double) cur->visit;
+		double rave_win_rate = (double) rave_map[cur->move].second / (double) rave_map[cur->move].first;
+		double exploitation = (1 - beta) * win_rate + beta * rave_win_rate;
+		double exploration = sqrt(log((double)cur->parent->visit)/cur->visit);
+		return exploitation + constant * exploration;
+	}
+	
+	void Expansion(node* parent_node, int& total_node) {
+		action::place child_move;
 		if (parent_node->who == board::black) {
-			// expand all the valid moves in white space 
-			for (size_t i = 0; i < white_space.size(); i++){
+			for(const action::place& child_move : white_space) {
 				board after = parent_node->state;
-				if (white_space[i].apply(after) == board::legal){
-					Node* child_node = new Node;
+				if (child_move.apply(after) == board::legal) {
+					node* child_node = new node;
+					child_node->state = after;
+					child_node->parent = parent_node;
+					child_node->move = child_move;
 					child_node->who = board::white;
-					child_node->state = after;
-                    child_node->move = white_space[i];
-					child_node->parent = parent_node;
 					parent_node->children.emplace_back(child_node);
+					if (rave_map.find(child_node->move) == rave_map.end()) 
+						rave_map.insert(std::make_pair(child_node->move, std::make_pair(0, 0)));
 				}
 			}
 		}
-		else if(parent_node->who == board::white) {
-			// expand all the valid moves in black space 
-			for (size_t i = 0; i < black_space.size(); i++){
+		else if (parent_node->who == board::white) {
+			for(const action::place& child_move : black_space) {
 				board after = parent_node->state;
-				if (black_space[i].apply(after) == board::legal){
-					Node* child_node = new Node;
-					child_node->who = board::black;
+				if (child_move.apply(after) == board::legal) {
+					node* child_node = new node;
 					child_node->state = after;
-                    child_node->move = black_space[i];
 					child_node->parent = parent_node;
+					child_node->move = child_move;
+					child_node->who = board::black;
 					parent_node->children.emplace_back(child_node);
+					if (rave_map.find(child_node->move) == rave_map.end()) 
+						rave_map.insert(std::make_pair(child_node->move, std::make_pair(0, 0)));
 				}
 			}
-		}
-	}
-
-    board::piece_type Simulation(Node* node){
-		bool flag = true;
-		board state = node->state;
-		board::piece_type nodewho = node->who;
-		while(flag){
-			flag = false;
-            nodewho = (node->who == board::white ? board::black : board::white); /* whose turn */
-			if (nodewho == board::black){
-				// black randomly gets one of valid moves 
+		}	
+		total_node += parent_node->children.size();
+	}				
+	
+	board::piece_type Simulation(node* root) {
+		bool terminal = false;
+		board state = root->state;
+		board::piece_type nodewho = root->who;
+		while(!terminal) {
+			terminal = true;
+			nodewho = (nodewho == board::white ? board::black : board::white);
+			if (nodewho == board::black) {
 				std::shuffle(black_space.begin(), black_space.end(), engine);
 				for (size_t i = 0; i < black_space.size(); i++) {
 					board after = state;
 					if (black_space[i].apply(after) == board::legal) {
 						black_space[i].apply(state);
-						flag = true;
+						terminal = false;
 						break;
 					}
 				}
 			}
 			else if (nodewho == board::white){
-				// white randomly gets one of valid moves 
 				std::shuffle(white_space.begin(), white_space.end(), engine);
 				for (size_t i = 0; i < white_space.size(); i++) {
 					board after = state;
 					if (white_space[i].apply(after) == board::legal){
 						white_space[i].apply(state);
-						flag = true;
+						terminal = false;
 						break;
 					}
 				}
 			}
 		}
-		// no legal action, so the rival wins 
-        nodewho = (nodewho == board::white ? board::black : board::white);
-		return nodewho; // return the winner
+		return (nodewho == board::white ? board::black : board::white);
 	}
-
-    void BackPropagation(Node* root, Node* node, bool win) {
-		while(node != root){
-			// update the node’s # of wins and # of totals 
-			node->total++;
-			if(win) node->win++;
-			node = node->parent;
+	
+	void BackPropagation(node* root, node* cur, board::piece_type winner) {
+		while(cur != root) {
+			cur->visit += 1;
+			rave_map[cur->move].first += 1;
+			if(winner != root->who){
+				cur->win += 1;
+				rave_map[cur->move].second += 1;
+			}
+			cur = cur->parent;
+		}
+		root->visit += 1;
+		rave_map[root->move].first += 1;
+		if(winner != root->who) {
+			root->win += 1;
+			rave_map[root->move].first += 1;
 		}
 	}
-
-	double get_ucb_value(Node* node) {
-		double exploit = float(node->win) / node->total;
-		double explore = std::sqrt(log((double)node->parent->total)/(double)node->total);
-		return exploit + constant * explore;
+	
+	void run_MCTS(node* root, board::piece_type winner, int total_node){
+		for (int i = 0; i < simulation_count; i++) {
+			node* best_node = Selection(root);
+			Expansion(best_node, total_node);	
+			if(best_node->children.size() != 0){
+				std::shuffle(best_node->children.begin(), best_node->children.end(), engine);
+				winner = Simulation(best_node->children[0]);
+				BackPropagation(root, best_node->children[0], winner);
+			}
+			else{
+				winner = Simulation(best_node);
+				BackPropagation(root, best_node, winner);
+			}
+			count += 1;
+		}
 	}
-    action get_best_action(Node* node){
-		action best_action = action();
-		int max_count = -1;
-		// select the best action based on the visit count of child nodes. 
-		for(size_t i = 0; i < node->children.size(); i++){
-			if (node->children[i]->total > max_count){
-				max_count = node->children[i]->total;
-				best_action = node->children[i]->move;
+	
+	action get_action(node* root) {					
+		int child_idx = -1;
+		int max_visit = 0;
+		for(size_t i = 0; i < root->children.size(); ++i) {
+			if(root->children[i]->visit > max_visit) {
+				max_visit = root->children[i]->visit;
+				child_idx = i;
 			}
 		}
-		return best_action;
+		if(child_idx != -1) return root->children[child_idx]->move;
+		return action();
 	}
-
-    void delete_tree(Node* node){
-		if(!node->children.empty()){
-			for(size_t i = 0; i < node->children.size(); i++) {
-                delete_tree(node->children[i]);
-                delete(node->children[i]);
-            }
+	
+	void delete_tree(node* node) {
+		if(node->children.empty() == false) {
+			for(size_t i = 0; i < node->children.size(); ++i) {
+				delete_tree(node->children[i]);
+				if(node->children[i] != NULL)
+					free(node->children[i]);
+			}
 			node->children.clear();
 		}
+		return;
 	}
 
 private:
-    std::string search;
-	std::vector<action::place> space;
-    std::vector<action::place> white_space;
-    std::vector<action::place> black_space;
+	std::string search;
+	int simulation_count = 0;
+	int count = 0;
+	int thread_num = 4;
 	board::piece_type who;
-    double constant = std::sqrt(2);
-	/*
-    double time_management[36] = {0.5, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, 0.8, 
-                                  1.0, 1.0, 1.0, 1.0, 1.5, 1.5, 1.5, 1.5, 
-                                  2.0, 2.0, 2.0, 2.0, 1.5, 1.5, 1.5, 1.5, 
-                                  1.0, 1.0, 1.0, 1.0, 0.8, 0.8, 0.8, 0.8, 
-                                  0.5, 0.5, 0.5, 0.5};
-	*/
-	double time_management[36] = {5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 
-                                  5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 
-								  10.0, 10.0, 10.0, 10.0, 10.0, 10.0,
-								  12.0, 12.0, 12.0, 12.0, 12.0, 12.0, 
-								  10.0, 10.0, 10.0, 10.0, 10.0, 10.0,
-								  10.0, 10.0, 10.0, 5.0, 5.0, 1.0};
-								  
+	std::map<action::place, std::pair<int, int> > rave_map;
+	double time_management[36] = {	5.0, 5.0, 5.0, 5.0, 5.0, 5.0,
+									6.0, 5.0, 5.0, 5.0, 5.0, 5.0,
+									9.0, 9.0, 9.0, 9.0, 9.0, 9.0,
+									10.0, 10.0, 10.0, 10.0, 10.0, 10.0,
+									10.0, 10.0, 10.0, 10.0, 10.0, 10.0,
+									10.0, 10.0, 10.0, 10.0, 10.0, 1.0
+								};
 };
